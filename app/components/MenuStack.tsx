a6f55fac7c42e083b638b8ea1b93b601a3304685
Sai Srikanth <saisrikanth2007@gmail.com>
2025-12-02 16:40:04 +0000

    fix: simplified the routing and page structure

--- app/components/MenuStack.tsx
+++ app/components/MenuStack.tsx
@@ -4,23 +4,17 @@ import Link from "next/link";
 
 function MenuStack() {
   return (
-    <div className="container sm:flex items-center justify-between p-2">
-  <div className="title">
-    <Link className="font-sans text-xl font-semibold"  href="./">Sai Srikanth</Link>
-  </div>
+    <div className="container flex items-center justify-between p-2">
+      <div className="title">
+        <Link className="font-sans text-xl font-semibold"  href="./">Sai Srikanth</Link>
+      </div>
 
- <ul className="flex gap-6 py-3 sm:py-0 border-neutral-700 border-b-1 sm:border-0">
-     <li className="">
-      <Link className="text-neutral-400 text-base" href="">Blog</Link>
-    </li>
-    
-
-    <li>
-      <Link className="text-neutral-400 text-base" href="/contact">Socials</Link>
-
-    </li>
-  </ul>
-  </div>
+      <ul className="menu">
+        <li className="link1">
+          <Link className="text-neutral-500 text-base" href="https://kryptokanth.medium.com">Thoughts</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -61,9 +61,9 @@ export default function Home() {
   
 
   <section>
-    <div className="pb-7 sm:py-5 ">
+    <div className="pb-7 sm:py-2 ">
 
-      <article className="py-3">
+      <article className="py-2">
         <p className="text-neutral-300">
           I am a software developer. I build web applications, experiment with Machine Learning Models, and actively invest in financial markets. I love the internet, and <u>kanth.in</u> serves as my public key on the Web 2.0 space.
         </p>
@@ -77,6 +77,52 @@ export default function Home() {
             </Link>
           </div>*/}
       </article>
+
+
+      <br />
+
+              <div className="grid grid-flow-row auto-rows-max">
+
+       <Link className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between " href="https://x.com/srikan1h">
+
+        X.com <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-up-right mt-1 mx-1" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>
+      </Link>
+
+          <Link className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between " href="https://github.com/srikan1h">
+
+        GitHub <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-up-right mt-1 mx-1" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>
+
+
+      </Link>
+
+      <a className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between" href="mailto:ss2675@srmist.edu.in">
+
+        E-mail <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-up-right mt-1 mx-1" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>
+
+
+      </a>
+
+
+
+
+            <Link className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between" href="https://linkedin.com/in/saisrikanths">
+
+        LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-arrow-up-right mt-1 mx-1" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>
+
+
+      </Link>
+   
+
+ 
+</div>
 
 
 
