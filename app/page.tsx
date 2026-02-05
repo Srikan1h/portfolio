d04ee41dc17a30581eae010c1cf54260cb44fd0e
Sai Srikanth <saisrikanth2007@gmail.com>
2026-02-05 16:32:10 +0000

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
               <div className="grid grid-flow-row auto-rows-max">
 
-                            <Link className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between" href="https://linkedin.com/in/saisrikanths">
+                            <Link className="flex font-mono text-sm py-2 px-3 bg-neutral-800 rounded-md mb-2 justify-between" href="https://linkedin.com/in/saisrikanths/">
 
         LinkedIn <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-up-right mt-1 mx-1" viewBox="0 0 16 16">
   <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
