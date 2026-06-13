7bea54bda365a6274e353936fd0a0d9c451c3405
srikan1h <saisrikanth2007@gmail.com>
2026-06-13 18:28:43 +0530

    Refactor thoughts page to group posts by year

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,44 +1,29 @@
 import Link from "next/link";
 import { PostMeta } from "@/lib/posts";
-import getFormattedDate from "@/lib/getFormattedDate";
 
 type Props = {
   post: PostMeta;
+  year?: string;
 };
 
-export default function ListItem({ post }: Props) {
-  const { slug, title, date } = post;
-  const formattedDate = getFormattedDate(date);
+export default function ListItem({ post, year }: Props) {
+  const { slug, title } = post;
 
   return (
-
-
-
-<li className="list-unstyled pb-3">
-  <div className="d-flex">
-  <div className="flex-grow-1">
-    <Link
-        href={`/thoughts/${slug}`}
-        className="link-dark link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
-      >
-        {title}
-      </Link>
-  </div>
-  <div className="text-end">
-        <span className="">{formattedDate}</span>
-
-  </div>
-</div>
-
-
-
-
-
-
-  </li>
-
-
-
-
+    <li className="list-unstyled pb-3">
+      <div className="d-flex align-items-baseline">
+        <div className="text-secondary opacity-75" style={{ width: '4.5rem', flexShrink: 0 }}>
+          {year ? year : ""}
+        </div>
+        <div className="flex-grow-1">
+          <Link
+            href={`/thoughts/${slug}`}
+            className="link-dark link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
+          >
+            {title}
+          </Link>
+        </div>
+      </div>
+    </li>
   );
 }
--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,4 +1,4 @@
-import { getSortedPostsData } from "@/lib/posts";
+import { getSortedPostsData, PostMeta } from "@/lib/posts";
 import ListItem from "../components/ListItem";
 
 export const metadata = {
@@ -9,13 +9,32 @@ export const metadata = {
 export default function ThoughtsPage() {
   const posts = getSortedPostsData();
 
+  // Group posts by year
+  const groupedPosts: Record<string, PostMeta[]> = {};
+  posts.forEach((post) => {
+    const year = new Date(post.date).getFullYear().toString();
+    if (!groupedPosts[year]) {
+      groupedPosts[year] = [];
+    }
+    groupedPosts[year].push(post);
+  });
+
+  // Sort years in descending order
+  const sortedYears = Object.keys(groupedPosts).sort((a, b) => parseInt(b) - parseInt(a));
+
   return (
     <section>
       <div className="py-3">
         <ul className="p-0 m-0">
-          {posts.map((post) => (
-            <ListItem key={post.slug} post={post} />
-          ))}
+          {sortedYears.map((year) =>
+            groupedPosts[year].map((post, index) => (
+              <ListItem
+                key={post.slug}
+                post={post}
+                year={index === 0 ? year : undefined}
+              />
+            ))
+          )}
         </ul>
       </div>
     </section>
